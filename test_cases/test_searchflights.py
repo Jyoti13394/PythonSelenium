6bf0886d49d827468e01d5b51de13b661ce8c1a7
Jyoti Prasad <prasadjyoti836@gmail.com>
2023-08-23 23:18:11 +0530

    data driven

--- test_cases/test_searchflights.py
+++ test_cases/test_searchflights.py
@@ -6,9 +6,11 @@ from selenium.webdriver.common.by import By
 from pages.search_flights_results_page import ResultPage
 from pages.yatra_launch_page import LaunchPage
 from utilities.utils import Utils
+from ddt import ddt, data, unpack, file_data
 
 
 @pytest.mark.usefixtures("setup")
+@ddt
 class TestSearchAndVerifyFilter(softest.TestCase):
 
     @pytest.fixture(autouse=True)
@@ -17,16 +19,19 @@ class TestSearchAndVerifyFilter(softest.TestCase):
         self.rp = ResultPage(self.driver)
         self.ut = Utils()
 
-    def test_search_flights(self):
-        self.lp.searchFlights('New Delhi', 'New York', '24/08/2023')
+    # @data(("New Delhi", "JFK", "25/08/2023", "1 Stop"), ("BOM", "JFK", "31/08/2023", "2 Stops"))
+    # @unpack
+    @file_data("../test_data/testdata.json")
+    def test_search_flights(self, goingfrom, goingto, date, stops):
+        self.lp.searchFlights(goingfrom, goingto, date)
         self.lp.scroll_down()
-        self.rp.filter_flights('1 Stop')
+        self.rp.filter_flights(stops)
         all_stops1 = self.rp.get_flight_filter_result()
         self.ut.assrtListItemText(all_stops1, "1 Stop")
 
-    def test_search_flights_2(self):
-        self.lp.searchFlights('New Delhi', 'New York', '26/08/2023')
-        self.lp.scroll_down()
-        self.rp.filter_flights('2 Stops')
-        all_stops1 = self.rp.get_flight_filter_result()
-        self.ut.assrtListItemText(all_stops1, "2 Stops ")
+    # def test_search_flights_2(self):
+    #     self.lp.searchFlights('New Delhi', 'New York', '26/08/2023')
+    #     self.lp.scroll_down()
+    #     self.rp.filter_flights('2 Stops')
+    #     all_stops1 = self.rp.get_flight_filter_result()
+    #     self.ut.assrtListItemText(all_stops1, "2 Stops ")
